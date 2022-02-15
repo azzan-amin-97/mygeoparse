81868f97d198bdb1f04cd1aec932b79fd33a5c81
azzan-amin-97 <ibnuamin97@gmail.com>
2022-02-15 20:21:59 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mygeoparse",
-    version="0.0.1",
+    version="0.0.2",
     description='Malaysia Address Parser',
     long_description=long_description,
     long_description_content_type='text/markdown',
